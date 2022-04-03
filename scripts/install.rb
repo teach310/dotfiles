ad99716794934cd26cb41d6b46624727da49453f
Taichi Sato <okms310gmail.com>
2022-04-03 18:34:53 +0900

    modify install.rb
    home直下以外もインストールできるように。
    spaceをescapeするように

--- scripts/install.rb
+++ scripts/install.rb
@@ -1,14 +1,14 @@
-require "optparse"
+require 'optparse'
 
 def run
-  options = ARGV.getopts("", "dry-run")
+  options = ARGV.getopts('', 'dry-run')
   target_paths = find_all_target_path
-  create_symbolic_links(target_paths, options["dry-run"])
-  puts "complete!"
+  create_symbolic_links(target_paths, options['dry-run'])
+  puts 'complete!'
 end
 
 def find_all_target_path
-  Dir.glob('home/*', File::FNM_DOTMATCH).select { |path| File.file?(path) }
+  Dir.glob('home/**/*', File::FNM_DOTMATCH).filter_map { |path| path.gsub(/ /, '\ ') if File.file?(path) }
 end
 
 def create_symbolic_links(target_paths, dry_run)
@@ -17,16 +17,20 @@ def create_symbolic_links(target_paths, dry_run)
   target_paths.each do |target_path|
     src_path = "#{pwd}/#{target_path}"
     dest_path = "#{home_dir}#{target_path.delete_prefix('home')}"
-    if File.exist?(dest_path)
-      puts "Using #{dest_path}"
-    else
-      if dry_run
-        puts "ln -s #{src_path} #{dest_path}"
-      else
-        `ln -s #{src_path} #{dest_path}`
-        puts "Link #{dest_path}"
-      end
-    end
+    link(src_path, dest_path, dry_run)
+  end
+end
+
+def link(src_path, dest_path, dry_run)
+  if File.exist?(dest_path)
+    puts "Using #{dest_path}"
+    return
+  end
+  if dry_run
+    puts "ln -s #{src_path} #{dest_path}"
+  else
+    `ln -s #{src_path} #{dest_path}`
+    puts "Link #{dest_path}"
   end
 end
 
